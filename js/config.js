ad8a95655cc87f23b4fc2823a5f5caa38dfa4f9c
student <svaidee1>
2025-06-11 22:25:36 -0400

    user pool

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_uHb7wwwAkeb', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-1_uHb7wAkeb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'jj6dr02f4f4lkskgelu1vj1tj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
