60ea3041f4ee7ebf51379328b82168f28253a64c
student <svaidee1>
2025-06-11 22:13:11 -0400

    user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_uHb7wwwAkeb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'jj6dr02f4f4lkskgelo1vj1tjS', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
